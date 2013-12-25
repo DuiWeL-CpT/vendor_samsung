2e576105f12cc0ec5a671d8da0502bfd89048c5a
mrimp <mrimp@day-off.net>
2013-12-25 13:29:55 -0500

    Updated to kitkat

--- crespo4g/proprietary/Android.mk
+++ crespo4g/proprietary/Android.mk
@@ -14,7 +14,6 @@
 
 LOCAL_PATH:=$(call my-dir)
 
-ifeq ($(TARGET_DEVICE),crespo4g)
 include $(CLEAR_VARS)
 LOCAL_MODULE := WiMAXSettings
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
@@ -50,4 +49,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
-endif
